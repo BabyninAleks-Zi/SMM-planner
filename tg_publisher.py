e37682d2fc15ca2906be773b9177634f303204cb
KamenskiyIlya <kamenskj.ilya@gmail.com>
2026-01-22 23:07:14 +0300

    Написал скрипт для удаления постов в тг

--- tg_publisher.py
+++ tg_publisher.py
@@ -2,11 +2,21 @@ import telegram
 from environs import Env
 
 
-def publish_post_to_tg(text: str, photo, chat_id, bot):
+
+def publish_post_to_tg(chat_id, bot, text: str = '', photo = None):
+		if photo:
+			result = bot.send_photo(chat_id=chat_id, photo=photo, caption=text)
+			return result['message_id']
+		if not photo:
+			result = bot.send_message(chat_id=chat_id, text=text)
+			return result['message_id']
+
+
+def delete_post_from_tg(bot, chat_id, post_id):
 	try:
-		bot.send_photo(chat_id=chat_id, photo=photo, caption=text)
-		return True
+		result = bot.delete_message(chat_id=chat_id, message_id=post_id)
+		return result
 	except:
-		return False
+		print('ошибка')
 
 	# доделать отработку, если пользователь передает только текст или только фото
